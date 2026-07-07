ffdf1aac29b4a583baa0b90460cbaa48a04242ee
brendogazevedo <agbrendo@gmail.com>
2026-07-07 19:10:52 -0300

    ress

--- exercicios/exercicio01/index.js
+++ exercicios/exercicio01/index.js
@@ -1,5 +1,12 @@
 function transformNumbers(numbers) {
     //code aqui
+    const result = [];
+    
+    for(let i = 0; i < numbers.length; i++) {
+        result.push(Number(numbers[i]));
+    }
+
+    return result;
 }
 
 console.log(transformNumbers(['10', '30', '40']));
